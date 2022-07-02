1c24c22f91015f387b69e535d855045df5a27eaf
hujuu <out.of.the.base.sfc@gmail.com>
2022-07-02 20:06:10 +0900

    Update my-ssr-page.jsx

--- pages/my-ssr-page.jsx
+++ pages/my-ssr-page.jsx
@@ -1,12 +1,17 @@
-export default MySsrPage = () =>
-  function SsrPage() {
-    return <></>;
-  };
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function MySsrPage() {
+  return <>test</>;
+}
 
 const getFullReturnTo = (ctx) => {
   // TODO: implement getFullReturnTo based on the ctx.resolvedUrl, ctx.locale
   // and your next.config.js's basePath and i18n settings.
-  return "/login";
+  return "/my-ssr-page";
 };
 
 export const getServerSideProps = (ctx) => {
